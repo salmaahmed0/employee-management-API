af7b9d22e634726753165746cfc65ebffe45ea1c
Salma Ahmed <salma.ahmed@gmail.com>
2024-02-20 10:53:28 +0200

    add jpaRepo

--- src/main/java/com/example/spring/repository/EmployeeRepo.java
+++ src/main/java/com/example/spring/repository/EmployeeRepo.java
@@ -1,10 +1,11 @@
 package com.example.spring.repository;
 
 import com.example.spring.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface EmployeeRepo {
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
 
     List<Employee> findAll();
 
